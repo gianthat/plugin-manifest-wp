ab38642fa8e60bc11810d9458d5fad2ff2e9a202
Reid Burnett <reid@gianthatworks.com>
2019-09-03 11:51:48 -0500

    skip alpha.3

--- plugin-manifest-wp.php
+++ plugin-manifest-wp.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Plugin Manifest
  * Plugin URI:        pluginmanifest-wp.com
  * Description:       See WordPress plugin status at a glance, whenever, wherever.
- * Version:           1.0.0-alpha.3
+ * Version:           1.0.0-alpha.4
  * Author:            1905 New Media
  * Author URI:        1905newmedia.com
  * License:           GPL-2.0+
