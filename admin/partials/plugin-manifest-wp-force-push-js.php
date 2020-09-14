a664b947848f3867eeac6d302c118ff287c3c835
Reid Burnett <reid@gianthatworks.com>
2020-09-13 22:16:08 -0500

    change force push button to use .button-force class

--- admin/partials/plugin-manifest-wp-force-push-js.php
+++ admin/partials/plugin-manifest-wp-force-push-js.php
@@ -1,7 +1,7 @@
 <script type="text/javascript">
 	jQuery(document).ready(function(){
 		var ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
-		jQuery('#plugin_manifest_wp_force_push').click(function(){
+		jQuery('.button-force').click(function(){
 			var email_id = jQuery('#plugin_manifest_wp_email_address').val();
 			jQuery.ajax({
 				url : ajaxurl,
