f01abdba12f5ddd59c46622180610099441482ce
Reid Burnett <reid@gianthatworks.com>
2023-03-22 18:19:38 -0500

    add option to set notification email and name, and a force push feature

--- includes/plugin-manifest-options.php
+++ includes/plugin-manifest-options.php
@@ -9,16 +9,28 @@ function plugin_manifest_options_page_callback() {
     // Get the current options values
     $recipients = get_option('plugin_manifest_recipients', '');
     $interval = get_option('plugin_manifest_interval', 'weekly');
+    $notification_email = get_option('plugin_manifest_notification_email', '');
+    $notification_name = get_option('plugin_manifest_notification_name', '');
 
     // Save new options values if the form is submitted
     if (isset($_POST['submit'])) {
         $recipients = sanitize_text_field($_POST['recipients']);
         $interval = sanitize_text_field($_POST['interval']);
+        $notification_email = sanitize_email($_POST['notification_email']);
+        $notification_name = sanitize_text_field($_POST['notification_name']);
 
         update_option('plugin_manifest_recipients', $recipients);
         update_option('plugin_manifest_interval', $interval);
+        update_option('plugin_manifest_notification_email', $notification_email);
+        update_option('plugin_manifest_notification_name', $notification_name);
 
         echo '<div class="notice notice-success"><p>Options updated successfully.</p></div>';
+    }
+
+    // Run the plugin and send an email outside the set interval if the "Force Push" button is clicked
+    if (isset($_POST['force_push'])) {
+        plugin_manifest_send_email();
+        echo '<div class="notice notice-success"><p>Email report sent successfully.</p></div>';
     }
 
     // Render the options form
@@ -41,8 +53,19 @@ function plugin_manifest_options_page_callback() {
                         </select>
                     </td>
                 </tr>
+                <tr valign="top">
+                    <th scope="row">Notification Email:</th>
+                    <td><input type="email" name="notification_email" value="<?php echo esc_attr($notification_email); ?>" class="regular-text"></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Notification Name:</th>
+                    <td><input type="text" name="notification_name" value="<?php echo esc_attr($notification_name); ?>" class="regular-text"></td>
+                </tr>
             </table>
-            <p class="submit"><input type="submit" name="submit" class="button-primary" value="Save Changes"></p>
+            <p class="submit">
+                <input type="submit" name="submit" class="button-primary" value="Save Changes">
+                <input type="submit" name="force_push" class="button-secondary" value="Force Push">
+            </p>
         </form>
     </div>
     <?php
